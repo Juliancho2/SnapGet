dbc0b1a2614ca3da0057a9ddf2cf9e1282a46f9c
julian moreno <julianmoreno242@gmail.com>
2023-09-17 15:16:50 -0500

    Added static files

---
+++ src/static.ts
@@ -0,0 +1,25 @@
+export const features=[
+    {
+        title:' Download Without Watermark',
+        description:'Forget about intrusive watermarks. With TokGo, you can download TikTok videos without any logo or watermark on them.'
+    },
+    {
+        title:'Easy to Use',
+        description:'Our intuitive interface allows you to copy and paste the link of the TikTok video you want to download in a matter of seconds.'
+    },
+    {
+        title:' High Quality',
+        description:'Enjoy downloaded videos in their original quality, so you won`t miss a single detail of the creativity of TikTok s content creators.'
+    },
+    {
+        title:'Fast Download',
+        description:'TokGo offers fast download speeds, so you can enjoy your favorite videos without unnecessa ry waiting.'
+    },
+];
+
+export const contentHowDoUse=[
+    'Find a video on TikTok that you want to download.',
+    'Copy the video link.',
+    'Visit TokGo and paste the link into the designated field.',
+    'Click Download and you re done! Your video is ready for download.'
+];
