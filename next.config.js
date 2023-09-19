497320184fdddb682bec29ae244c1e066c12d3bd
julian moreno <julianmoreno242@gmail.com>
2023-09-18 21:47:50 -0500

    added image domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['p16-sign-va.tiktokcdn.com',
+      'p16-sign.tiktokcdn-us.com',
+      'p19-sign.tiktokcdn-us.com', 'p19-sign.tiktokcdn.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
