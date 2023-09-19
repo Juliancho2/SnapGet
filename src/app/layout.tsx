e5654d30001b316c835c515b3b27c279bfa6015f
julian moreno <julianmoreno242@gmail.com>
2023-09-18 22:59:14 -0500

    Added analitycs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 
-const inter = Poppins({ subsets: ['latin'],weight:['100','200','300','400','500','600','700'] });
+const inter = Poppins({ subsets: ['latin'], weight: ['100', '200', '300', '400', '500', '600', '700'] });
 
 export const metadata: Metadata = {
   title: 'TokGo',
   description: 'Your Source for TikTok Videos without Watermark',
-  icons:'logo.svg'
+  icons: 'logo.svg'
 };
 
 export default function RootLayout({
@@ -17,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics/>
+      </body>
     </html>
   );
 }
