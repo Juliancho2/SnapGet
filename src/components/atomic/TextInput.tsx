c3463d40d4cd2a831586a548cda1240fcbfa9d57
julian moreno <julianmoreno242@gmail.com>
2023-09-18 21:44:42 -0500

    Changed styles and added pasteFromClipboard functionality.

--- src/components/atomic/TextInput.tsx
+++ src/components/atomic/TextInput.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import Image from 'next/image';
+import React, { SetStateAction } from 'react';
+import paste from 'src/assets/svg/paste.svg';
+import close from 'src/assets/svg/close.svg';
 
-const TextInput = () => {
+type Props = {
+  setUrl: React.Dispatch<SetStateAction<string>>,
+  url: string,
+  loading: boolean | undefined
+}
+
+const TextInput = ({ setUrl, url, loading }: Props) => {
+
+  const pasteFromClipboard = async () => {
+      const text = await navigator.clipboard.readText();
+      setUrl(text);
+};
+
   return (
-    <div className='bg-white  min-h-[40px] w-4/5 flex p-2 rounded-md shadow-sm'>
-        <input type="text " className='bg-transparent h-full w-full outline-none' placeholder='Paste your link'/>
-        <button className='btn btn-neutral'>Download</button>
+    <div className='bg-white h-full w-full min-h-[40px]  flex p-2 rounded-md shadow-sm'>
+      <input value={url} required onChange={(e) => setUrl(e.target.value)} type="text " className='bg-transparent h-full w-full outline-none' placeholder='Paste your link' />
+      {
+        url.trim().length === 0 ?
+        <div className="tooltip flex items-center justify-center mx-2 hover:opacity-75 cursor-pointer transition-all duration-200" data-tip="paste">
+          <Image onClick={pasteFromClipboard} src={paste} alt='paste' width={40} height={40} />
+        </div>
+        :
+        <div className="tooltip flex items-center justify-center mx-2 hover:opacity-75 cursor-pointer transition-all duration-200" data-tip="remove">
+          <Image onClick={()=>setUrl('')} src={close} alt='paste' width={40} height={40} />
+        </div>
+
+
+      }
+      <button className='btn btn-neutral'>
+        {
+          loading ? <span className="loading loading-dots loading-lg"></span>
+            : 'Download'
+        }
+      </button>
     </div>
   );
 };
