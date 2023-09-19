d240724aa6716b03a34517ac9fef030266289034
julian moreno <julianmoreno242@gmail.com>
2023-09-18 21:42:31 -0500

    Added result and provider component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,26 @@
 'use client';
-import { useEffect } from 'react';
 import Features from '@/components/organisms/Features';
 import Footer from '@/components/organisms/Footer';
 import Header from '@/components/organisms/Header';
 import Hero from '@/components/organisms/Hero';
+import Result from '@/components/organisms/Result';
+import {  DataInfoProvider } from '@/context/infoVideoContext';
 
 
 export default function Home() {
 
   return (
     <>
-      <Header />
-      <main className='min-h-screen mt-5'>
-        <Hero />
-        <Features/>
-      </main>
-      <Footer />
+      <DataInfoProvider>
+        <Header />
+        <main className='min-h-screen mt-5'>
+          <Hero />
+          <Result/>
+          <Features  />
+
+        </main>
+        <Footer />
+      </DataInfoProvider>
     </>
   );
 }
