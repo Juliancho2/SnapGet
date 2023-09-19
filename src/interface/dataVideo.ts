66ef2d1008177bd337fa641a0838b1db4bc33d9e
julian moreno <julianmoreno242@gmail.com>
2023-09-18 21:45:33 -0500

    Added interfaces

---
+++ src/interface/dataVideo.ts
@@ -0,0 +1,29 @@
+export interface VideoInfo {
+    video: string[];
+    music: string[];
+    cover: string[];
+    OriginalWatermarkedVideo: string[];
+    description: string[];
+    dynamic_cover: string[];
+    author: string[];
+    region: string[];
+    avatar_thumb: string[];
+    custom_verify: string[];
+    videoid: string[];
+    post_type: string;
+}
+
+export interface ErrorVideo{
+    name: string,
+    isError: boolean
+
+}
+
+export interface DataVideoContextType {
+    dataVideo:VideoInfo | undefined,
+    handleDataVideo:(value:any)=>void,
+    handleLoadingVideo:()=>void,
+    handleError:(value:string)=>void,
+    loading:boolean,
+    error:ErrorVideo
+  }
