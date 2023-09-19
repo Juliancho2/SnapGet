cdea80a6d956f3d9deb2088330ffc3d6b326bbd9
julian moreno <julianmoreno242@gmail.com>
2023-09-18 21:45:03 -0500

    The text was modified

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className='bg-black'>
+
       <div className='max-w-6xl mx-auto text-xs flex flex-col items-center text-gray-300 py-5 space-y-2'>
         <p>Made by @julianchoms</p>
-        <p>We are not affiliated with TikTok, Douyin or Bytedance.</p>
+        <p>We are not affiliated with TikTok.</p>
         <p>Copyright 2023</p>
       </div>
     </footer>
