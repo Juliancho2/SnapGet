8f65df5fa3b34511a485963ead8806859fa01af9
julian moreno <julianmoreno242@gmail.com>
2023-09-20 21:59:16 -0500

    Added validation for the url

--- src/components/molecules/Download.tsx
+++ src/components/molecules/Download.tsx
@@ -1,41 +1,38 @@
 'use client';
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import axios from 'axios';
 import Text from '../atomic/Text';
 import TextInput from '../atomic/TextInput';
-import List from '../atomic/List';
-import BoxText from './BoxText';
-import { contentHowDoUse } from '@/static';
 import { DataInfoContext } from '@/context/infoVideoContext';
 import { DataVideoContextType } from '@/interface/dataVideo';
 
-
 const Download = () => {
-  const [url, setUrl] = useState('');
   const context = useContext<DataVideoContextType | undefined>(DataInfoContext);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     const regx = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/ig;
+    const regex = /tiktok/g;
     e.preventDefault();
 
-    if (!regx.test(url)) {
-     return context?.handleError('Invalid Url');
+    if (context) {
+      const apiURl = !regex.test(context?.urlInput) ? '/api/all-media' : '/api/tiktok';
+
+      if (!regx.test(context?.urlInput)) {
+        return context?.handleError('Invalid Url');
+      }
+      try {
+        context?.handleLoadingVideo();
+        const response = await axios.post(apiURl, { url: context.urlInput });
+        context?.handleDataVideo(response.data);
+      } catch (err) {
+        context?.handleError('An error occurred try again later');
+      }
     }
-    try {
-      context?.handleLoadingVideo();
-      const response = await axios.post('/api/downloader', { url });
-      context?.handleDataVideo(response.data);
-    }
-    catch (err) {
-      context?.handleError('An error occurred try again later');
-    }
-    setUrl('');
     context?.handleLoadingVideo();
   };
 
-
   return (
-    <section className='grid md:grid-cols-2 min-h-[70vh]'>
+    <section >
       {
         context?.error.isError && (
           <div className="toast toast-end">
@@ -45,31 +42,15 @@ const Download = () => {
           </div>
         )
       }
-      <div className='flex flex-col items-start gap-10 justify-center mb-10'>
-        <Text styles='text-4xl font-semibold text-center md:text-start '>Your Source for TikTok Videos <br /> without Watermark</Text>
-        <form className='w-full md:w-4/5' onSubmit={(e) => handleSubmit(e)}>
-          <TextInput loading={context?.loading} setUrl={setUrl} url={url} />
-        </form>
-      </div>
-      <div className='flex flex-col items-end justify-center'>
-        <BoxText>
-          <h2 className='text-2xl font-semibold mb-10'>Are you a TikTok lover?</h2>
-          <Text styles='text-sm text-black' >
+      {
+        context && <div className='flex flex-col items-start gap-10 justify-center mt-10'>
+          <Text styles='text-4xl font-semibold text-center md:text-start  '>Your source of videos  from your favorite social networks</Text>
+          <form className='w-full md:w-4/5 h-auto' onSubmit={(e) => handleSubmit(e)}>
+            <TextInput loading={context?.loading} setUrl={context?.handleUrlInput} url={context?.urlInput} />
+          </form>
+        </div>
+      }
 
-            have you ever found
-            an amazing video you want to save, share or
-            enjoy without the TikTok watermark, you re in
-            the right place! TokGo is your ultimate solution
-            to download TikTok videos quickly and easily,
-            removing the watermark and giving you full
-            control over the content you love.
-          </Text>
-        </BoxText>
-        <BoxText>
-          <h2 className='text-2xl font-semibold mb-10'>how do to use?</h2>
-          <List content={contentHowDoUse} />
-        </BoxText>
-      </div>
     </section>
   );
 };
--- src/hooks/useMediaDownload.tsx
+++ src/hooks/useMediaDownload.tsx
@@ -1,27 +1,29 @@
-import React, { useState } from 'react';
+import { useContext, useState } from 'react';
 import { VideoInfo } from '@/interface/dataVideo';
+import { DataInfoContext } from '@/context/infoVideoContext';
 
+const useMediaDownload = (data: VideoInfo) => {
+  const context = useContext(DataInfoContext);
+  const [urlVideo, setUrlVideo] = useState('');
 
-const useMediaDownload = ( data :VideoInfo ) => {
-    const [urlVideo, setUrlVideo] = useState('');
-    const [urlAudio, setUrlAudio] = useState('');
+  const handleDownloadClick = () => {
+    const regex = /tiktok/g;
 
-    const handleDownloadClick = () => {
-        const videoPromise = fetch(data.video[0])
-            .then((response) => response.blob())
-            .then((blob) => window.URL.createObjectURL(blob));
+    if (context && !regex.test(context.urlInput)) {
+      setUrlVideo(data.video[0]);
+    } else {
+      const videoPromise = fetch(data.video[0])
+        .then((response) => response.blob())
+        .then((blob) => window.URL.createObjectURL(blob));
 
-        const audioPromise = fetch(data.music[0])
-            .then((response) => response.blob())
-            .then((blob) => window.URL.createObjectURL(blob));
-
-        Promise.all([videoPromise, audioPromise])
-            .then(([videoUrl, audioUrl]) => {
-                setUrlVideo(videoUrl);
-                setUrlAudio(audioUrl);
-            });
-    };
-    return { urlVideo, urlAudio, handleDownloadClick };
+      Promise.all([videoPromise])
+        .then(([videoUrl]) => {
+          setUrlVideo(videoUrl);
+        });
+    }
+    context?.handleUrlInput('');
+  };
+  return { urlVideo, handleDownloadClick };
 };
 
 export default useMediaDownload;
