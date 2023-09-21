750aea14b56f98b6c37b56cb081aec0e939fce89
julian moreno <julianmoreno242@gmail.com>
2023-09-20 22:02:29 -0500

    Added section Supported platforms

--- src/components/organisms/Features.tsx
+++ src/components/organisms/Features.tsx
@@ -1,15 +1,47 @@
-
 import React from 'react';
+import tiktok from 'src/assets/svg/tiktok.svg';
+import yt from 'src/assets/svg/youtube.svg';
+import facebook from 'src/assets/svg/facebook.svg';
+import instagram from 'src/assets/svg/instagram.svg';
+import Image from 'next/image';
+import BoxText from '../molecules/BoxText';
+import Text from '../atomic/Text';
 import { features } from '@/static';
 
+const platormAllowed = [
+  {
+    label: 'Tiktok',
+    icon: tiktok
+  },
+  {
+    label: 'Youtube',
+    icon: yt
+  },
+  {
+    label: 'Instagram',
+    icon: instagram
+  },
+  {
+    label: 'Facebook',
+    icon: facebook
+  }
+];
+
 const Features = () => {
-    return (
-        <div className='max-w-6xl mx-auto space-y-7 min-h-[50vh] px-2'>
+  return (
+        <div className='max-w-6xl mx-auto my-5 md:my-20 space-y-28 min-h-[50vh] px-2'>
+                <BoxText >
+                    <h2 className='text-2xl font-semibold mb-5 text-center md:text-start'>Are you a social media enthusiast?</h2>
+                    <Text styles='text-base text-black text-center' >
+
+                        Have you ever come across incredible videos that you want to save, share, or enjoy without annoying watermarks? You`re in luck! DownloadPro is your ultimate solution for quickly and easily downloading videos from TikTok, Facebook, Instagram, and YouTube while removing watermarks. With DownloadPro, you`ll have full control over the content you cherish.
+                    </Text>
+                </BoxText>
             <section>
                 <div className="join join-vertical w-full bg-white">
                     {features.map((item, index) => (
                         <div key={index} className="collapse collapse-arrow join-item border border-base-300">
-                            <input type="radio" name="my-accordion-4"/>
+                            <input type="radio" name="my-accordion-4" />
                             <div className="collapse-title text-xl font-medium">
                                 {item.title}
                             </div>
@@ -20,8 +52,25 @@ const Features = () => {
                     ))}
                 </div>
             </section>
+            <section className='flex flex-col items-center'>
+                <h2 className='text-2xl font-semibold mb-10 text-center'>Supported platforms</h2>
+                <div className="stats shadow stats-vertical md:stats-horizontal">
+                    {
+                        platormAllowed.map(item => (
+
+                            <div key={item.label} className="stat flex flex-col gap-3">
+                                <div className="stat-figure text-primary">
+                                    <Image width={50} height={50} src={item.icon} alt={item.label} ></Image>
+                                </div>
+                                <div className="stat-value text-black text-lg">{item.label}</div>
+                            </div>
+                        ))
+                    }
+
+                </div>
+            </section>
         </div>
-    );
+  );
 };
 
 export default Features;
