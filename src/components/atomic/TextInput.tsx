5f5127e3c1c380369ba4454815786c095c9a0853
julian moreno <julianmoreno242@gmail.com>
2023-09-20 22:00:22 -0500

    Added status to handle url in context

--- src/components/atomic/TextInput.tsx
+++ src/components/atomic/TextInput.tsx
@@ -1,39 +1,37 @@
 import Image from 'next/image';
-import React, { SetStateAction } from 'react';
+import React from 'react';
 import paste from 'src/assets/svg/paste.svg';
 import close from 'src/assets/svg/close.svg';
 
 type Props = {
-  setUrl: React.Dispatch<SetStateAction<string>>,
+  setUrl: (value: string) => void
   url: string,
   loading: boolean | undefined
 }
 
 const TextInput = ({ setUrl, url, loading }: Props) => {
-
   const pasteFromClipboard = async () => {
-      const text = await navigator.clipboard.readText();
-      setUrl(text);
-};
+    const text = await navigator.clipboard.readText();
+    setUrl(text);
+  };
 
   return (
-    <div className='bg-white h-full w-full min-h-[40px]  flex p-2 rounded-md shadow-sm'>
-      <input value={url} required onChange={(e) => setUrl(e.target.value)} type="text " className='bg-transparent h-full w-full outline-none' placeholder='Paste your link' />
+    <div className='bg-white h-[64px] w-full flex p-2 rounded-md shadow-sm'>
+      <input value={url} required onChange={(e) => setUrl(e.target.value)} type="text " className='h-full w-full outline-none' placeholder='Paste your link' />
       {
-        url.trim().length === 0 ?
-        <div className="tooltip flex items-center justify-center mx-2 hover:opacity-75 cursor-pointer transition-all duration-200" data-tip="paste">
+       url.trim().length === 0
+         ? <div className="tooltip flex items-center justify-center mx-2 hover:opacity-75 cursor-pointer transition-all duration-200" data-tip="paste">
           <Image onClick={pasteFromClipboard} src={paste} alt='paste' width={40} height={40} />
         </div>
-        :
-        <div className="tooltip flex items-center justify-center mx-2 hover:opacity-75 cursor-pointer transition-all duration-200" data-tip="remove">
-          <Image onClick={()=>setUrl('')} src={close} alt='paste' width={40} height={40} />
+         : <div className="tooltip flex items-center justify-center mx-2 hover:opacity-75 cursor-pointer transition-all duration-200" data-tip="remove">
+          <Image onClick={() => setUrl('')} src={close} alt='paste' width={40} height={40} />
         </div>
-
 
       }
       <button className='btn btn-neutral'>
         {
-          loading ? <span className="loading loading-dots loading-lg"></span>
+          loading
+            ? <span className="loading loading-dots loading-lg"></span>
             : 'Download'
         }
       </button>
--- src/context/infoVideoContext.tsx
+++ src/context/infoVideoContext.tsx
@@ -1,8 +1,7 @@
 import React, { createContext, ReactNode, useState } from 'react';
 import { DataVideoContextType } from '@/interface/dataVideo';
 
-
-const initialError={name:'',isError:false};
+const initialError = { name: '', isError: false };
 
 // Crear el contexto
 export const DataInfoContext = createContext<DataVideoContextType | undefined>(undefined);
@@ -11,25 +10,28 @@ export const DataInfoProvider: React.FC<{ children: ReactNode }> = ({ children }
   const [dataVideo, setDataVideo] = useState(undefined);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(initialError);
+  const [urlInput, setUrlInput] = useState('');
 
+  const handleUrlInput = (value:string) => {
+    setUrlInput(value);
+  };
 
-    const handleDataVideo=(value:any)=>{
-        setDataVideo(value);
-    };
+  const handleDataVideo = (value:any) => {
+    setDataVideo(value);
+  };
 
-    const handleLoadingVideo=()=>{
-        setLoading((prev)=>!prev);
-    };
+  const handleLoadingVideo = () => {
+    setLoading((prev) => !prev);
+  };
 
-    const handleError=(err:string)=>{
-      setError({name:err,isError:true});
+  const handleError = (err:string) => {
+    setError({ name: err, isError: true });
 
-      setTimeout(()=>{setError(initialError);},2000);
-    };
-
+    setTimeout(() => { setError(initialError); }, 2000);
+  };
 
   return (
-    <DataInfoContext.Provider value={{ dataVideo,handleDataVideo,loading,handleLoadingVideo,handleError,error }}>
+    <DataInfoContext.Provider value={{ dataVideo, handleDataVideo, loading, handleLoadingVideo, handleError, error, urlInput, handleUrlInput }}>
       {children}
     </DataInfoContext.Provider>
   );
