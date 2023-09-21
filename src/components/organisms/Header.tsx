b2719c6c9bdbce7d5f8ef6e61458f551030a7e7e
julian moreno <julianmoreno242@gmail.com>
2023-09-21 15:16:42 -0500

    Added sticky position

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -4,7 +4,7 @@ import NavBar from '../molecules/NavBar';
 
 const Header = () => {
   return (
-    <header className='py-5 px-2 w-full '>
+    <header className='py-5 px-2 w-full sticky top-0 z-40 '>
       <div className='flex justify-between container max-w-6xl mx-auto bg-white w-full rounded-md shadow-sm px-4'>
         <section className='flex  items-center'>
           <Logo width={40} height={40} />
