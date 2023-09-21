39119315a467ec34bfcb07d509d3738772072fc8
julian moreno <julianmoreno242@gmail.com>
2023-09-20 21:40:03 -0500

    Added new images domains

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   images: {
     domains: ['p16-sign-va.tiktokcdn.com',
       'p16-sign.tiktokcdn-us.com',
-      'p19-sign.tiktokcdn-us.com', 'p19-sign.tiktokcdn.com'],
-  },
+      'p19-sign.tiktokcdn-us.com', 'p19-sign.tiktokcdn.com', 'https://scontent-cdg4-2.xx.fbcdn.net/', 'p16-va.tiktokcdn.com', 'scontent.fbne7-1.fna.fbcdn.net', 'img.youtube.com', 'snapxcdn.com', 'scontent.fhan14-2.fna.fbcdn.net', 'scontent.fhan20-1.fna.fbcdn.net', 'scontent.fhan14-1.fna.fbcdn.net']
+  }
 };
 
 module.exports = nextConfig;
