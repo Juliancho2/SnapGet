0beb547cf19e0d876e1ae624fe3e4db2814173d3
julian moreno <julianmoreno242@gmail.com>
2023-09-21 15:16:15 -0500

    The hook has been refactored

--- src/hooks/useMediaDownload.tsx
+++ src/hooks/useMediaDownload.tsx
@@ -6,23 +6,32 @@ const useMediaDownload = (data: VideoInfo) => {
   const context = useContext(DataInfoContext);
   const [urlVideo, setUrlVideo] = useState('');
 
-  const handleDownloadClick = () => {
-    const regex = /tiktok/g;
+  const handleDownloadClick = async () => {
+    try {
+      const regex = /tiktok/g;
 
-    if (context && !regex.test(context.urlInput)) {
-      setUrlVideo(data.video[0]);
-    } else {
-      const videoPromise = fetch(data.video[0])
-        .then((response) => response.blob())
-        .then((blob) => window.URL.createObjectURL(blob));
+      if (context && !regex.test(context.urlInput)) {
+        setUrlVideo(data.video[0]);
+      } else {
+        const response = await fetch(data.video[0]);
 
-      Promise.all([videoPromise])
-        .then(([videoUrl]) => {
-          setUrlVideo(videoUrl);
-        });
+        if (!response.ok) {
+          throw new Error('No se pudo descargar el video.');
+        }
+
+        const blob = await response.blob();
+        const videoUrl = window.URL.createObjectURL(blob);
+
+        setUrlVideo(videoUrl);
+      }
+
+      // Limpia el contexto después de procesar el enlace
+      context?.handleUrlInput('');
+    } catch (error) {
+      throw new Error('An unexpected error occurred. Please try again later.');
     }
-    context?.handleUrlInput('');
   };
+
   return { urlVideo, handleDownloadClick };
 };
 
