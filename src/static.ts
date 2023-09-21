20b6c44c197ff0edffa19d238cc1c86edd6154d3
julian moreno <julianmoreno242@gmail.com>
2023-09-20 22:00:38 -0500

    Added new features

--- src/static.ts
+++ src/static.ts
@@ -1,25 +1,30 @@
-export const features=[
-    {
-        title:' Download Without Watermark',
-        description:'Forget about intrusive watermarks. With TokGo, you can download TikTok videos without any logo or watermark on them.'
-    },
-    {
-        title:'Easy to Use',
-        description:'Our intuitive interface allows you to copy and paste the link of the TikTok video you want to download in a matter of seconds.'
-    },
-    {
-        title:' High Quality',
-        description:'Enjoy downloaded videos in their original quality, so you won`t miss a single detail of the creativity of TikTok s content creators.'
-    },
-    {
-        title:'Fast Download',
-        description:'TokGo offers fast download speeds, so you can enjoy your favorite videos without unnecessa ry waiting.'
-    },
+export const features = [
+  {
+    title: 'Download from Multiple Platforms',
+    description: 'Access a wide range of social platforms and download videos from TikTok, YouTube, Instagram, Facebook, and more with just one click.'
+  },
+  {
+    title: 'High-Quality Downloads',
+    description: 'Download videos in the best available quality for an optimal viewing experience.'
+  },
+
+  {
+    title: ' Download Without Watermark',
+    description: 'You can download videos from TikTok without any logo or watermark on them.'
+  },
+  {
+    title: 'Easy to Use',
+    description: 'Our intuitive interface allows you to copy and paste the link of the video you want to download in a matter of seconds.'
+  },
+  {
+    title: 'Fast Download',
+    description: 'SnapGet offers fast download speeds, so you can enjoy your favorite videos without unnecessa ry waiting.'
+  }
 ];
 
-export const contentHowDoUse=[
-    'Find a video on TikTok that you want to download.',
-    'Copy the video link.',
-    'Visit TokGo and paste the link into the designated field.',
-    'Click Download and you re done! Your video is ready for download.'
+export const contentHowDoUse = [
+  'Find a video on SnapGet, Facebook, Youtube or yout that you want to download.',
+  'Copy the video link.',
+  'Visit SnapGet and paste the link into the designated field.',
+  'Click Download and you re done! Your video is ready for download.'
 ];
