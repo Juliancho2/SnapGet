9ebb94beca90a2ff5ad6b83e85b6eced11e0d61d
julian moreno <julianmoreno242@gmail.com>
2023-09-20 22:02:55 -0500

    Changed styles and text

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='bg-black'>
+    <footer className='bg-black rounded-md p-1 min-h-[15vh]'>
 
-      <div className='max-w-6xl mx-auto text-xs flex flex-col items-center text-gray-300 py-5 space-y-2'>
+      <div className='max-w-6xl mx-auto text-xs flex flex-col items-center text-gray-300 py-5 space-y-2 '>
         <p>Made by @julianchoms</p>
-        <p>We are not affiliated with TikTok.</p>
+        <p>We are not affiliated with any of the supported platforms.</p>
         <p>Copyright 2023</p>
       </div>
     </footer>
--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import Logo from '../atomic/Logo';
 import NavBar from '../molecules/NavBar';
 
-
 const Header = () => {
   return (
     <header className='py-5 px-2 w-full '>
       <div className='flex justify-between container max-w-6xl mx-auto bg-white w-full rounded-md shadow-sm px-4'>
         <section className='flex  items-center'>
           <Logo width={40} height={40} />
-          <h1 className='text-sm font-bold text-black'>TokGo</h1>
+          <h1 className='text-sm font-bold text-black'>SnapGet</h1>
         </section>
         <NavBar/>
 
