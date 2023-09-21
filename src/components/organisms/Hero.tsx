bd4fb89c17219f37c73ea7717fcb13d3bb3333e8
julian moreno <julianmoreno242@gmail.com>
2023-09-20 22:05:03 -0500

    Added section how do to use

--- src/components/organisms/Hero.tsx
+++ src/components/organisms/Hero.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Download from '../molecules/Download';
+import BoxText from '../molecules/BoxText';
+import List from '../atomic/List';
+import { contentHowDoUse } from '@/static';
 
 const Hero = () => {
   return (
-    <div className='max-w-6xl mx-auto px-2'>
-      <Download/>
+    <div className='max-w-6xl mx-auto px-2 mt-10 md:mt-32 gap-10 grid md:grid-cols-2 min-h-[40vh] '>
+
+      <Download />
+      <div className='flex justify-end'>
+        <BoxText moreStyles='max-w-md  p-2 ' >
+          <h2 className='text-2xl font-semibold mb-5'>how do to use?</h2>
+          <List content={contentHowDoUse} />
+        </BoxText>
+      </div>
     </div>
   );
 };
