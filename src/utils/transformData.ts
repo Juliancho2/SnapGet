94a41eb508fc5c3641ec4bc2f4d918ba3e56d866
julian moreno <julianmoreno242@gmail.com>
2023-09-21 15:15:35 -0500

    Added validation in case any property is not defined.

--- src/utils/transformData.ts
+++ src/utils/transformData.ts
@@ -1,11 +1,11 @@
 export const transformData = (originalData:any) => {
   const transformedData = {
-    video: [originalData.links[1].link],
+    video: [originalData.links[1] ? originalData.links[1].link : originalData.links[0].link],
     music: [''],
-    cover: [originalData.picture],
-    OriginalWatermarkedVideo: [originalData.links[0].link],
-    description: [originalData.description],
-    dynamic_cover: [originalData.picture],
+    cover: [originalData.picture ? originalData.picture : ''],
+    OriginalWatermarkedVideo: [''],
+    description: [originalData.description ? originalData.description : ''],
+    dynamic_cover: [originalData.picture ? originalData.picture : ''],
     author: [''],
     region: [''],
     avatar_thumb: [''],
